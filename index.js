04aa7d7ca8bdf9a1157ce15d5957a74fb13aed20
Linghua Jin <ljin@walmartlabs.com>
2015-04-30 13:24:09 -0700

    improved middleware, delete functionality

--- index.js
+++ index.js
@@ -7,9 +7,10 @@
 let path = require('path')
 let fs = require('fs')
 let express = require('express')
-let nodeify = require('bluebird-nodeify')  //to convert promise to callbacks
+let nodeify = require('bluebird-nodeify') //to convert promise to callbacks
 let morgan = require('morgan')
 let mime = require('mime-types')
+let rimraf = require('rimraf')
 
 //to allow use of promise
 require('songbird')
@@ -22,25 +23,58 @@ let app = express()
 
 //morgan runs first, then run other app.get, or actions
 //depends on declear sequence
-if (NODE_ENV === 'development'){
-	app.use(morgan('dev'))
+if (NODE_ENV === 'development') {
+    app.use(morgan('dev'))
 }
 
 app.listen(PORT, () => console.log(`LISTENING http://localhost:${PORT}`))
 
-app.get('*', sendHeaders, (req, res) => {
-	//if directory, we set it to body 
-	//ToDO: improve so less hacky
-	if (res.body){
-		return res.json(res.body)
-	}
+/**
+ *  curl -v 'http://localhost:8000/' --get
+ */
+app.get('*', setFileMeta, sendHeaders, (req, res) => {
+    //if directory, we set it to body 
+    //ToDO: improve so less hacky
+     if (req.stat && req.stat.isDirectory){
+        return res.json(res.body)
+    }
 
-	fs.createReadStream(req.filePath).pipe(res);
+    fs.createReadStream(req.filePath).pipe(res);
 })
+
 // curl -v http://localhost:8000/ -X HEAD
-app.head('*', sendHeaders, (req, res) => {
+app.head('*', setFileMeta, sendHeaders, (req, res) => {
 
 });
+
+
+app.delete('*', setFileMeta, (req, res, next) => {
+    //only call next if it fails
+    async() => {
+       if (req.stat && req.stat.isDirectory){
+       	await rimraf.promise(req.filePath)
+       }
+    }().catch(next)
+});
+
+/**
+ * set file path
+ * @param {[type]}   req  [description]
+ * @param {[type]}   res  [description]
+ * @param {Function} next [description]
+ */
+function setFileMeta(req, res, next) {
+    let filePath = path.resolve(path.join(ROOT_DIR, req.url));
+    if (filePath.indexOf(ROOT_DIR) !== 0) {
+        return res.send(400, 'invalid path')
+    }
+    req.filePath = filePath; //so you can pass via middle ware to next actions(middleware)
+
+    // next();
+    fs.promise.stat(filePath)
+        .then(stat => req.stat = stat, () => req.stat = null)
+        .nodeify(next)//chain promise to resolve cb
+}
 /**
  * send headers serves as middleware
  * @param  {[type]}   req  [description]
@@ -48,32 +82,30 @@ app.head('*', sendHeaders, (req, res) => {
  * @param  {Function} next [description]
  * @return {[type]}        [description]
  */
-function sendHeaders(req, res, next){
-	//convert promise back to callback
-	//take this promise and connect to next callback
-	nodeify(async ()=> {
-		//TODO: handle if there is not url
-		let filePath = path.resolve(path.join(ROOT_DIR, req.url));
-		req.filePath = filePath;//so you can pass via middle ware to next actions(middleware)
-		if (filePath.indexOf(ROOT_DIR)!==0){
-			return res.send(400, 'invalid path')
-		}
-
-		let stat = await fs.promise.stat(filePath)
-		if (stat.isDirectory()){
-			let files = await fs.promise.readdir(filePath)
-			res.body = JSON.stringify(files);
-			res.setHeader('Content-Length',res.body.length);
-			res.setHeader('Content-Type', 'application/json')
-			return
-		}
-		//if stat is file
-		else{
-			let contentType = mime.contentType(path.extname(filePath))
-			res.setHeader('Content-Length',stat.size)
-			res.setHeader('Content-Type', contentType)
-		}
-	}(), next);
+function sendHeaders(req, res, next) {
+    //convert promise back to callback
+    //take this promise and connect to next callback
+    //no matter error or succss, always go next
+    nodeify(async() => {
+    	if (req.stat){
+    		      //TODO: handle if there is not url
+		        if (req.stat.isDirectory()) {
+		            let files = await fs.promise.readdir(req.filePath)
+		            res.body = JSON.stringify(files);
+		            res.setHeader('Content-Length', res.body.length);
+		            res.setHeader('Content-Type', 'application/json')
+		            return
+		        }
+		        //if stat is file
+		        else {
+		            let contentType = mime.contentType(path.extname(req.filePath))
+		            res.setHeader('Content-Length', req.stat.size)
+		            res.setHeader('Content-Type', contentType)
+		        }
+    	
+    	}
+  
+    }(), next);
 }
 // function sendHeaders(req, res, next) {
 //   // send headers logic
@@ -97,7 +129,7 @@ function sendHeaders(req, res, next){
 //     stream.on('error', function(err){
 //     	 res.end(err);
 //     })
-  
+
 // });
 
 // //curl --head http://localhost:8000
